a6e7b9b31a72f4995a30923affddcf0ccfed3361
Sanket Thorat <sanketthorat352@gmail.com>
2024-10-23 19:26:28 +0530

    Add files via upload

---
+++ Practise day 10/StringBuilder.java
@@ -0,0 +1,17 @@
+//Delete char at some Index
+import java.util.*;
+
+
+public class StringBuilder {
+   public static void main(String args[]) {
+     StringBuilder sb = new StringBuilder("tony");
+     //Insert char
+     sb.insert(0, 'S');
+     System.out.println(sb);
+
+
+	//delete char
+     sb.delete(0, 1);
+     System.out.println(sb);
+   }
+}
