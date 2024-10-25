0138fcffd07714b23b35855b83ed1097686a8c90
Sanket Thorat <sanketthorat352@gmail.com>
2024-10-25 14:09:09 +0530

    Add files via upload

---
+++ Practise Day 11/Bit.java
@@ -0,0 +1,18 @@
+//get bit
+import java.util.*;
+
+
+public class Bit {
+   public static void main(String args[]) {
+      int n = 5; //0101
+      int pos = 3;
+      int bitMask = 1<<pos;
+
+
+      if((bitMask & n) == 0) {
+          System.out.println("bit was zero");
+      } else {
+          System.out.println("bit was one");
+      }
+   }
+}
